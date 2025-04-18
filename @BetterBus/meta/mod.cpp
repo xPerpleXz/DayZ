48cbeca6d505e0676c6baf3931f79e836298ad71
xPerpleXz <137960973+xPerpleXz@users.noreply.github.com>
2025-04-18 04:51:34 +0200

    Add files via upload (#1) (#2)

---
+++ @BetterBus/meta/mod.cpp
@@ -0,0 +1,8 @@
+name = "TransitBus Mod";
+picture = "";
+logo = "";
+logoOver = "";
+tooltip = "TransitBus Mod";
+overview = "Stellt den originalen DayZ TransitBus als nutzbares Fahrzeug ins Spiel. Nutzt ausschließlich Vanilla-Ressourcen.";
+author = "xPerpleXz";
+version = "1.2";
