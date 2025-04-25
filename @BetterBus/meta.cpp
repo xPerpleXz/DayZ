c5958fe68dfe9bafe7c4f622c8203e03d59d4a9f
xPerpleXz <137960973+xPerpleXz@users.noreply.github.com>
2025-04-25 18:10:19 +0200

    Delete @BetterBus directory

--- @BetterBus/meta.cpp
+++
@@ -1,4 +0,0 @@
-protocol = 1;
-publishedid = 3465731414;
-name = "BetterBus";
-timestamp = 5250491386354557972;
--- @BetterBus/meta/mod.cpp
+++
@@ -1,8 +0,0 @@
-name = "TransitBus Mod";
-picture = "";
-logo = "";
-logoOver = "";
-tooltip = "TransitBus Mod";
-overview = "Stellt den originalen DayZ TransitBus als nutzbares Fahrzeug ins Spiel. Nutzt ausschließlich Vanilla-Ressourcen.";
-author = "xPerpleXz";
-version = "1.2";
